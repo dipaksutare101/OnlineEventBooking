04c1b5f7e5f5c5b4b0cddeca97f75abf298ce6ab
dipak <dip@3sixtyit.com>
2019-11-30 14:10:49 +0530

    Comit

---
+++ OnlineEventBooking/Controllers/RegisterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+namespace OnlineEventBooking.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Register()
+        {
+            using (EventDBEntities db= new EventDBEntities())
+            {
+                var country = db.Countries.ToList();
+                List<SelectListItem> dd = new List<SelectListItem>();
+                dd = country;
+                ViewBag.countrylist = dd;
+            }
+                return View();
+        }
+
+        public JsonResult StateList(int id)
+        {
+            using (EventDBEntities db= new EventDBEntities())
+            {
+                var State = db.States.Where(x => x.CountryID == id).ToList();
+                return Json(State, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
