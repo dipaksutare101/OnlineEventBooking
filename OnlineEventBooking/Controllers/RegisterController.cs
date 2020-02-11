cf1ceca846bfe75050ecef477ab20eca3680e310
dipak <dip@3sixtyit.com>
2020-02-11 18:52:19 +0530

    Commit

--- OnlineEventBooking/Controllers/RegisterController.cs
+++ OnlineEventBooking/Controllers/RegisterController.cs
@@ -16,17 +16,39 @@ namespace OnlineEventBooking.Controllers
                 var country = new SelectList(db.Countries.ToList(), "CountryID","Name");
                
                 ViewBag.countrylist = country;
+
+                var role = new SelectList(db.Roles.ToList(), "RoleID", "Rolename");
+
+                ViewBag.Rolelist = role;
             }
                 return View();
         }
 
-        public JsonResult StateList()
+        public JsonResult StateList(int id)
         {
             using (EventDBEntities db= new EventDBEntities())
             {    
-                var State = new SelectList(db.States.ToList(), "StateID", "StateName");
+                var State = new SelectList(db.States.Where(x=>x.CountryID ==id).ToList(), "StateID", "StateName");
                 return Json(State, JsonRequestBehavior.AllowGet);
             }
+        }
+        public JsonResult CityList(int id)
+        {
+            using (EventDBEntities db = new EventDBEntities())
+            {
+                var CityList = new SelectList(db.Cities.Where(x => x.StateID == id).ToList(), "CityID", "CityName");
+                return Json(CityList, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Save(Registration reg)
+        {
+            using (EventDBEntities db= new EventDBEntities())
+            {
+                db.Registrations.Add(reg);
+                db.SaveChanges();
+            }
+                return View("Register");
         }
     }
 }
